ade9b2af955d78e0867eb212025559f6699df6ff
Samuel Adebayo <adebayosamueloluwafemi1@gmail.com>
2025-08-01 20:54:28 +0100

    Add first and second code

--- Week 1 - Python-Git/Day 1/first-code.py
+++ Week 1 - Python-Git/Day 1/first-code.py
@@ -2,7 +2,7 @@ from netmiko import ConnectHandler
 
 router = {
     'device_type': 'cisco_ios',
-    'host': '192.168.220.50',
+    'host': '192.168.220.53',
     'username': 'Samuel',
     'password': 'Samuel@123',
     'port': 22
