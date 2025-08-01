485eccf3cd4aeb530363d3bab8135e13c40f36f4
Samuel Adebayo <adebayosamueloluwafemi1@gmail.com>
2025-08-01 19:10:43 +0100

    Add first Python script for Week 1 Day 1

---
+++ Week 1/Day 1/first-code.py
@@ -0,0 +1,14 @@
+from netmiko import ConnectHandler
+
+router = {
+    'device_type': 'cisco_ios',
+    'host': '192.168.220.50',
+    'username': 'Samuel',
+    'password': 'Samuel@123',
+    'port': 22
+}
+
+ssh = ConnectHandler(**router)
+ssh.enable()
+result = ssh.send_command('show ip int b')
+print(result)
