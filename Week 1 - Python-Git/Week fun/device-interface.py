179faba77740e85e4acd3c89e34d284240a3c6fd
Samuel Adebayo <adebayosamueloluwafemi1@gmail.com>
2025-08-02 14:07:30 +0100

    Add fun code - Device/interface mapping

---
+++ Week 1 - Python-Git/Week fun/device-interface.py
@@ -0,0 +1,20 @@
+# This code receives device name and interface in a list
+# Group them in dictionary mapping - devices and interface
+
+list = [
+    "Access-01_e0/0", "Access-02_e0/0", "Access-03_e0/0", "Access-04_e0/0", 
+    "Access-05_e0/1", "Access-06_e0/1", "Access-07_e0/1", "Access-08_e0/1", 
+    "Access-09_e0/2", "Access-10_e0/2", "Access-11_e0/2", "Access-12_e0/2"
+]
+
+devices = []
+interfaces = []
+
+for i in list:
+    device = i.split('_')[0]
+    devices.append(device)
+    interface = i.split('_')[-1]
+    interfaces.append(interface)
+    
+final_dict = {'devices':devices, 'interfaces':interfaces}
+print(final_dict)
